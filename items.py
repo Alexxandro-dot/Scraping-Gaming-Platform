bae4e882ef242b58806d8c1e00e9e8817001fac4
Alexxandro-dot <62535768+Alexxandro-dot@users.noreply.github.com>
2020-03-24 13:46:33 +0100

    Update items.py

--- items.py
+++ items.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 from scrapy.loader.processors import TakeFirst, MapCompose, Join
 from w3lib.html import remove_tags
